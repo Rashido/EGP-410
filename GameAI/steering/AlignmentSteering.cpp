d22e6a70f7c415ff524260a6551ae8aeec91d3e9
Pereyra <gabriel.pereyra@CHAMPLAIN.EDU>
2017-04-19 20:47:10 -0400

    First commit for our Architecture Project. Redid all the math in AlignmentSteering.cpp

--- GameAI/steering/AlignmentSteering.cpp
+++ GameAI/steering/AlignmentSteering.cpp
@@ -22,8 +22,36 @@ Steering* AlignmentSteering::getSteering()
 			if ((mpMover->getPosition() - it.second->getPosition()).getLength() <= mAlignRadius &&
 				(mpMover->getPosition() - it.second->getPosition()).getLength() >= -mAlignRadius)
 			{
-				mLinear += it.second->getVelocity();
-				++neighborCount;
+				//ASSEMBLY HERE
+				float tmpLinearX = mLinear.mX; float tmpLinearY = mLinear.mY;
+				float tmpVelX = it.second->getVelocity().mX; float tmpVelY = it.second->getVelocity().mY;
+
+				float count = 1;
+
+				__asm
+				{
+					movss xmm0, tmpLinearX
+					movss xmm1, tmpVelX
+					addss xmm0, xmm1
+					movss tmpLinearX, xmm0
+
+					movss xmm0, tmpLinearY
+					movss xmm1, tmpVelY
+					addss xmm0, xmm1
+					movss tmpLinearY, xmm0	
+
+					movss xmm0, neighborCount
+					movss xmm1, count
+					addss xmm0, xmm1
+					movss neighborCount, xmm0
+
+				}
+
+				mLinear.mX = tmpLinearX;
+				mLinear.mY = tmpLinearY;
+
+				//mLinear += it.second->getVelocity();
+				//++neighborCount;
 			}
 		}
 	}
@@ -31,7 +59,64 @@ Steering* AlignmentSteering::getSteering()
 	if (neighborCount == 0)
 		return this;
 
-	mLinear /= neighborCount;
-	mLinear.normalize();
+	//MORE ASSEMBLY
+	float tmpLinearX = mLinear.mX; float tmpLinearY = mLinear.mY;
+	
+	__asm
+	{
+		movss xmm0, tmpLinearX
+		movss xmm1, neighborCount
+		divss xmm0, xmm1
+		movss tmpLinearX, xmm0
+
+		movss xmm0, tmpLinearY
+		movss xmm1, neighborCount
+		divss xmm0, xmm1
+		movss tmpLinearY, xmm0
+	}
+
+	mLinear.mX = tmpLinearX;
+	mLinear.mY = tmpLinearY;
+	//mLinear /= neighborCount;
+
+	//EVEN MORE ASSEMBLY
+	tmpLinearX = mLinear.mX; tmpLinearY = mLinear.mY;
+	float magnitude;
+	__asm
+	{
+		movss xmm0, tmpLinearX
+		movss xmm1, tmpLinearX
+		mulss xmm0, xmm1
+		movss tmpLinearX, xmm0
+
+		movss xmm0, tmpLinearY
+		movss xmm1, tmpLinearY
+		mulss xmm0, xmm1
+		movss xmm1, xmm0
+
+		movss xmm0, tmpLinearX
+		addss xmm0, xmm1
+		sqrtss xmm0, xmm0
+		movss magnitude, xmm0
+
+	}
+
+	tmpLinearX = mLinear.mX;
+	tmpLinearY = mLinear.mY;
+
+	__asm
+	{
+		movss xmm0, tmpLinearX
+		divss xmm0, magnitude
+		movss tmpLinearX, xmm0
+
+		movss xmm0, tmpLinearY
+		divss xmm0, magnitude
+		movss tmpLinearY, xmm0
+	}
+
+	mLinear.mX = tmpLinearX;
+	mLinear.mY = tmpLinearY;
+	//mLinear.normalize();
 	return this;
 }
